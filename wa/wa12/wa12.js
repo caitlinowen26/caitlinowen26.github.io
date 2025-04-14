4826c2d5962d64f5229e06b2e682fe0d8fe2183c
Caitlin Owen <caitlin.owen@colorado.edu>
2025-04-13 22:36:33 -0600

    WebDev 12

---
+++ wa/wa12/wa12.js
@@ -0,0 +1,54 @@
+// Select the new quote button
+const newQuoteButton = document.querySelector('#js-new-quote');
+const answerButton = document.querySelector('#js-tweet');
+
+// Add an event listener to the button
+newQuoteButton.addEventListener('click', getQuote);
+answerButton.addEventListener('click', getAnswer);
+
+const quoteText = document.querySelector('#js-quote-text');
+const answerText = document.querySelector('#js-answer-text');
+
+// API endpoint
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion'; //change api
+
+// Function to fetch a new quote
+async function getQuote() 
+{
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) 
+        {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+        const data = await response.json();
+        displayQuote(data.question);
+        answer = data.answer;
+    } 
+    catch (error) 
+    {
+        console.error("Failed to fetch quote:", error);
+        alert("Failed to fetch a new quote. Please check the console for details.");
+    }
+}
+
+async function getAnswer() 
+{
+    displayAnswer(answer);
+}
+
+// Function to display the quote in the HTML
+function displayQuote(quote) 
+{
+    quoteText.textContent = quote;
+}
+
+function displayAnswer(answer) 
+{
+    answerText.textContent = answer;
+}
+
+// Fetch a quote on page load
+getQuote();
+
+let answer = "";
